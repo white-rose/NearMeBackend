b2e21d50daf601e2b99b4e87911cb3670ac52e62
Nathan Nguyen <nathanthainguyen@gmail.com>
2018-03-11 14:54:14 -0700

    Add Locationtag

---
+++ src/main/java/model/LocationTag.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.sql.Timestamp;
+
+public abstract class LocationTag {
+
+    String facebookId;
+    String locality;
+    Timestamp timestamp;
+
+    public LocationTag(String facebookId, String locality, Timestamp timestamp) {
+        this.facebookId = facebookId;
+        this.locality = locality;
+        this.timestamp = timestamp;
+    }
+
+    public String getFacebookId() {
+        return facebookId;
+    }
+
+    public void setFacebookId(String facebookId) {
+        this.facebookId = facebookId;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+
+}
